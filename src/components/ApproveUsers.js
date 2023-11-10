df5d15f8d672a184a80d989c3b74e75c305cbcbc
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-11-09 22:55:49 -1000

    update for UI

--- src/components/ApproveUsers.js
+++ src/components/ApproveUsers.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import UserService from '../services/user.service';
-import {Container} from 'react-bootstrap';
+import {Container, Table, Button, Alert } from 'react-bootstrap';
 import axios from 'axios';
-
 
 const ApproveUsers = () => {
     const [users, setUsers] = useState([]);
@@ -11,7 +10,7 @@ const ApproveUsers = () => {
     const [approvedUsers, setApprovedUsers] = useState({});
     const [approvedUserEmails, setApprovedUserEmails] = useState([]);
     const [message, setMessage] = useState('');
-
+    const [selectAll, setSelectAll] = useState(false);
 
     const fetchData = async () => {
         try {
@@ -26,9 +25,11 @@ const ApproveUsers = () => {
             setLoading(false);
         }
     };
+
     useEffect(() => {
         fetchData();
     }, []);
+
     const sendEmail = (approvedUserEmails) => {
         axios
             .post('http://localhost:8080/sendEmailToApprovedUsers', {
@@ -45,10 +46,8 @@ const ApproveUsers = () => {
     };
 
     const handleCheckboxChange = (userId) => {
-        //console.log('Checkbox clicked for user ID:', userId);
         const isUserApproved = !approvedUsers[userId];
 
-        // Update the approvedUsers state
         setApprovedUsers((prevApprovedUsers) => ({
             ...prevApprovedUsers,
             [userId]: isUserApproved,
@@ -57,87 +56,117 @@ const ApproveUsers = () => {
         setApprovedUserEmails((prevEmails) => {
             const emailToUpdate = users.find((user) => user.id === userId).email;
             if (isUserApproved) {
-                console.log('Adding email:', emailToUpdate);
                 return [...prevEmails, emailToUpdate];
             } else {
-                console.log('Removing email:', emailToUpdate);
                 return prevEmails.filter((email) => email !== emailToUpdate);
             }
         });
 
+        const allSelected = users.every((user) => approvedUsers[user.id]);
+        setSelectAll(allSelected);
+        if (!isUserApproved) {
+            setSelectAll(false);
+        }
     };
+
+    const handleSelectAllChange = () => {
+        const newSelectAll = !selectAll;
+        setSelectAll(newSelectAll);
+
+        const updatedApprovedUsers = {};
+        users.forEach((user) => {
+            updatedApprovedUsers[user.id] = newSelectAll;
+        });
+        setApprovedUsers(updatedApprovedUsers);
+
+        const emailsToUpdate = newSelectAll
+            ? users.map((user) => user.email)
+            : [];
+        setApprovedUserEmails(emailsToUpdate);
+    };
+
     const approveUsers = async () => {
-        const approvedUserIds = users.filter((user) => approvedUsers[user.id])
+        const approvedUserIds = users
+            .filter((user) => approvedUsers[user.id])
             .map((user) => user.id);
-        console.log("Approved User IDs:", approvedUserIds);
+        console.log('Approved User IDs:', approvedUserIds);
 
         if (approvedUserIds.length > 0) {
             try {
-                const updatedApprovedUsers = {...approvedUsers};
-                approvedUserIds.forEach((userId) => {
-                    updatedApprovedUsers[userId] = true;
-                });
-                setApprovedUsers(updatedApprovedUsers);
-
                 await UserService.updatedApprovedStatus(approvedUserIds);
                 sendEmail(approvedUserEmails);
                 await fetchData();
             } catch (err) {
-                console.error("Error approving users:", err);
+                console.error('Error approving users:', err);
             }
         }
-    }
-
+    };
 
     if (loading) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
+
     if (error) {
         return <div>Error: {error.message}</div>;
     }
 
     return (
-        <div className="user-contents">
-            <div className="user-table">
-                <h2>Unapproved Users</h2>
-                <div className="user-cards">
-                    {users.map((user) => (
-                        <div className="user-card" key={user.id}>
-                            <div className="card-header">
-                                <h3>
-                                    {`${user.first_name} ${user.last_name}`}
-                                </h3>
-                            </div>
-                            <div className="card-content">
-                                <p>
-                                    <strong>Email: </strong>
-                                    {user.email}
-                                </p>
-                                <p>
-                                    <strong>Phone: </strong>
-                                    {user.phone_number}
-                                </p>
-                                <p>
-                                    <strong>Company: </strong>
-                                    {user.company}
-                                </p>
+        <Container>
+            <h2 className="mt-5 mb-4">Unapproved Users</h2>
+            {users.length > 0 ? (
+                <div>
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Company</th>
+                            <th>
                                 <label>
                                     <input
                                         type="checkbox"
-                                        checked={approvedUsers[user.id] || false}
-                                        onChange={() => handleCheckboxChange(user.id)}
+                                        checked={selectAll}
+                                        onChange={handleSelectAllChange}
                                     />
-                                    Approve
+                                    Select All
                                 </label>
-                            </div>
-                        </div>
-                    ))}
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {users.map((user) => (
+                            <tr key={user.id}>
+                                <td>{`${user.first_name} ${user.last_name}`}</td>
+                                <td>{user.email}</td>
+                                <td>{user.phone_number}</td>
+                                <td>{user.company}</td>
+                                <td>
+                                    <label className="checkbox-label">
+                                        <input
+                                            type="checkbox"
+                                            checked={approvedUsers[user.id] || false}
+                                            onChange={() => handleCheckboxChange(user.id)}
+                                        />
+                                        Approve
+                                    </label>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </Table>
+                    <div className="d-flex justify-content-end mt-3">
+                        <Button onClick={approveUsers}>Submit</Button>
+                    </div>
+                </div>) : (
+                <div>
+                    <Alert variant="info" className="mt-5">
+                        No new users need to be approved
+                    </Alert>
                 </div>
-                <button onClick={approveUsers}>Submit</button>
-            </div>
-        </div>
+            )}
+        </Container>
     );
 };
 
 export default ApproveUsers;
-
