a82605dac31d8a6cf5a6f4c5ae8df605249392ef
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-11-11 19:02:57 -1000

    save exist UI for chat

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-import { Grid, GridItem, Box, Input, Flex, Button, Text } from '@chakra-ui/react';
+
+import './Chatting.css';
+import AuthService from '../services/auth.service';
 
 const ENDPOINT = 'http://localhost:8080';
 
 const Chatting = () => {
+    const currentUser = AuthService.getCurrentUser();
     const [socket, setSocket] = useState(null);
     const [inputMessage, setInputMessage] = useState('');
     const [room, setRoom] = useState('');
-    const [selectedFriend, setSelectedFriend] = useState(null);
-    const [username, setUsername] = useState("")
     const [showChat, setShowChat] = useState(false);
     const [messageList, setMessageList] = useState([]);
     const [value, setValue] = useState(new Date());
+    const user = 'feimei@hawaii.edu';
+    const userName = `feimei`;
 
     useEffect(() => {
         const interval = setInterval(() => setValue(new Date()), 1000);
@@ -21,14 +24,13 @@ const Chatting = () => {
             clearInterval(interval);
         };
     }, []);
-    const joinRoom = ()=>{
-        if(username !== "" && room !==""){
-            socket.emit("join room", room)
+
+    const joinRoom = () => {
+        if (room !== "") {
+            socket.emit('join room', room);
             setShowChat(true);
-
         }
-    }
-
+    };
 
     useEffect(() => {
         const newSocket = io(ENDPOINT);
@@ -44,24 +46,23 @@ const Chatting = () => {
         if (socket) {
             // Listen for incoming messages
             socket.on('receive message', (message) => {
-                console.log(message);
-                setMessageList((list)=>[...list, message]);
+                setMessageList((list) => [...list, message]);
             });
         }
     }, [socket]);
 
     const handleSendMessage = async () => {
-        //if (socket && inputMessage.trim() !== '' && room.trim() !== '') {
         if (socket && inputMessage.trim() !== '') {
             const messageData = {
                 room: room,
-                author:username,
-                messages:inputMessage,
-                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
+                author: userName,
+                messages: inputMessage,
+                time: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes(),
             };
+
             // Send the message to the server along with the room name
             await socket.emit('chat message', messageData);
-            setMessageList((list)=>[...list, messageData]);
+            setMessageList((list) => [...list, messageData]);
 
             // Clear the input field
             setInputMessage('');
@@ -69,70 +70,55 @@ const Chatting = () => {
     };
 
     return (
-        <Grid templateColumns="repeat(12, 1fr)" h="100vh">
+        <div className="chat-container">
             {/* Friends List on the left side */}
-            <GridItem colSpan={3} borderRight="1px solid gray" p={4}>
-                <Text fontWeight="bold" mb={4}>
-                    Start to chat with others
-                </Text>
-                {/* Add your friends list or other content here */}
-                <Flex direction="column">
-                    <h3>Join A chat</h3>
-                    <Input
-                        type="text"
-                        placeholder="Enter friends name"
-                        value={username}
-                        onChange={(event)=> {setUsername(event.target.value)}}
-                    />
-                    <Input
-                        type="text"
-                        placeholder="Enter room name"
-                        value={room}
-                        onChange={(event)=> {setRoom(event.target.value)}}
-                    />
-                    <Button onClick={joinRoom}>Join Room</Button>
-                </Flex>
+            <div className="friends-list">
+                <h3>Join A chat</h3>
+                <input
+                    type="text"
+                    placeholder="Enter room name"
+                    value={room}
+                    onChange={(event) => {
+                        setRoom(event.target.value);
+                    }}
+                />
+                <button onClick={joinRoom}>Join Room</button>
+            </div>
 
-            </GridItem>
-            <GridItem colSpan={9} p={4}>
-                {showChat ? (
-                    <Flex direction="column" h="100%">
-                        {/* Chat Messages */}
-                        <h1>Room {room}</h1>
-                        <Box flex="1" overflowY="auto">
-                            {messageList.map((message) => (
-                                <Box
-                                    key={message.id}
-                                    textAlign={message.author === username ? 'right' : 'left'}
-                                    mb={2}
-                                >
-                                    <Text fontWeight="bold">
-                                        {message.author}: {message.messages}
-                                    </Text>
-                                </Box>
-                            ))}
-                        </Box>
+            {/* Chat Messages */}
+            {showChat ? (
+                <div className="chat-messages">
+                    <h1>Room {room}</h1>
+                    <div className="message-container">
+                        {messageList.map((message) => (
+                            <div
+                                key={message.id}
+                                className={`message ${message.author === user ? 'own-message' : 'other-message'}`}
+                            >
+                                <div className="message-header">
+                                    <span className="username">{message.username}</span>
+                                    <span className="time">{message.time}</span>
+                                </div>
+                                <div className="message-content">{message.messages}</div>
+                            </div>
+                        ))}
+                    </div>
 
-                        {/* Message Input and Send Button */}
-                        <Flex>
-                            <Input
-                                type="text"
-                                placeholder="Type message here"
-                                value={inputMessage}
-                                onChange={(e) => setInputMessage(e.target.value)}
-                            />
-                            <Button ml={2} onClick={handleSendMessage}>
-                                Send
-                            </Button>
-                        </Flex>
-                    </Flex>
-                ) : (
-                    <p>
-                        Enter a room to chat with other users, and do not let users know your chat room key.
-                    </p>
-                )}
-            </GridItem>
-        </Grid>
+                    {/* Message Input and Send Button */}
+                    <div className="message-input">
+                        <input
+                            type="text"
+                            placeholder="Type message here"
+                            value={inputMessage}
+                            onChange={(e) => setInputMessage(e.target.value)}
+                        />
+                        <button onClick={handleSendMessage}>Send</button>
+                    </div>
+                </div>
+            ) : (
+                <p>Enter a room to chat with other users, and do not let users know your chat room key.</p>
+            )}
+        </div>
     );
 };
 
--- src/components/Chatting.js
+++ src/components/Chatting.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-import { Grid, GridItem, Box, Input, Flex, Button, Text } from '@chakra-ui/react';
+
+import './Chatting.css';
 import AuthService from '../services/auth.service';
 
 const ENDPOINT = 'http://localhost:8080';
@@ -10,13 +11,11 @@ const Chatting = () => {
     const [socket, setSocket] = useState(null);
     const [inputMessage, setInputMessage] = useState('');
     const [room, setRoom] = useState('');
-    const [selectedFriend, setSelectedFriend] = useState(null);
     const [showChat, setShowChat] = useState(false);
     const [messageList, setMessageList] = useState([]);
     const [value, setValue] = useState(new Date());
     const user = currentUser.email;
     const userName = `${currentUser.first_name} ${currentUser.last_name}`;
-
 
     useEffect(() => {
         const interval = setInterval(() => setValue(new Date()), 1000);
@@ -25,14 +24,13 @@ const Chatting = () => {
             clearInterval(interval);
         };
     }, []);
-    const joinRoom = ()=>{
-        if(room !==""){
-            socket.emit("join room", room)
+
+    const joinRoom = () => {
+        if (room !== "") {
+            socket.emit('join room', room);
             setShowChat(true);
-
         }
-    }
-
+    };
 
     useEffect(() => {
         const newSocket = io(ENDPOINT);
@@ -48,25 +46,24 @@ const Chatting = () => {
         if (socket) {
             // Listen for incoming messages
             socket.on('receive message', (message) => {
-                console.log(message);
-                setMessageList((list)=>[...list, message]);
+                setMessageList((list) => [...list, message]);
             });
         }
     }, [socket]);
 
     const handleSendMessage = async () => {
-        //if (socket && inputMessage.trim() !== '' && room.trim() !== '') {
         if (socket && inputMessage.trim() !== '') {
             const messageData = {
                 room: room,
-                author:currentUser.email,
+                author: user,
                 username: userName,
-                messages:inputMessage,
-                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
+                messages: inputMessage,
+                time: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes(),
             };
+
             // Send the message to the server along with the room name
             await socket.emit('chat message', messageData);
-            setMessageList((list)=>[...list, messageData]);
+            setMessageList((list) => [...list, messageData]);
 
             // Clear the input field
             setInputMessage('');
@@ -74,64 +71,55 @@ const Chatting = () => {
     };
 
     return (
-        <Grid templateColumns="repeat(12, 1fr)" h="100vh">
+        <div className="chat-container">
             {/* Friends List on the left side */}
-            <GridItem colSpan={3} borderRight="1px solid gray" p={4}>
-                <Text fontWeight="bold" mb={4}>
-                    Start to chat with others
-                </Text>
-                {/* Add your friends list or other content here */}
-                <Flex direction="column">
-                    <h3>Join A chat</h3>
-                    <Input
-                        type="text"
-                        placeholder="Enter room name"
-                        value={room}
-                        onChange={(event)=> {setRoom(event.target.value)}}
-                    />
-                    <Button onClick={joinRoom}>Join Room</Button>
-                </Flex>
+            <div className="friends-list">
+                <h3>Join A chat</h3>
+                <input
+                    type="text"
+                    placeholder="Enter room name"
+                    value={room}
+                    onChange={(event) => {
+                        setRoom(event.target.value);
+                    }}
+                />
+                <button onClick={joinRoom}>Join Room</button>
+            </div>
 
-            </GridItem>
-            <GridItem colSpan={9} p={4}>
-                {showChat ? (
-                    <Flex direction="column" h="100%">
-                        {/* Chat Messages */}
-                        <h1>Room {room}</h1>
-                        <Box flex="1" overflowY="auto">
-                            {messageList.map((message) => (
-                                <Box
-                                    key={message.id}
-                                    textAlign={message.author === user ? 'right' : 'left'}
-                                    mb={2}
-                                >
-                                    <Text fontWeight="bold">
-                                        {message.username}: {message.messages}
-                                    </Text>
-                                </Box>
-                            ))}
-                        </Box>
+            {/* Chat Messages */}
+            {showChat ? (
+                <div className="chat-messages">
+                    <h1>Room {room}</h1>
+                    <div className="message-container">
+                        {messageList.map((message) => (
+                            <div
+                                key={message.id}
+                                className={`message ${message.author === user ? 'own-message' : 'other-message'}`}
+                            >
+                                <div className="message-header">
+                                    <span className="username">{message.username}</span>
+                                    <span className="time">{message.time}</span>
+                                </div>
+                                <div className="message-content">{message.messages}</div>
+                            </div>
+                        ))}
+                    </div>
 
-                        {/* Message Input and Send Button */}
-                        <Flex>
-                            <Input
-                                type="text"
-                                placeholder="Type message here"
-                                value={inputMessage}
-                                onChange={(e) => setInputMessage(e.target.value)}
-                            />
-                            <Button ml={2} onClick={handleSendMessage}>
-                                Send
-                            </Button>
-                        </Flex>
-                    </Flex>
-                ) : (
-                    <p>
-                        Enter a room to chat with other users, and do not let users know your chat room key.
-                    </p>
-                )}
-            </GridItem>
-        </Grid>
+                    {/* Message Input and Send Button */}
+                    <div className="message-input">
+                        <input
+                            type="text"
+                            placeholder="Type message here"
+                            value={inputMessage}
+                            onChange={(e) => setInputMessage(e.target.value)}
+                        />
+                        <button onClick={handleSendMessage}>Send</button>
+                    </div>
+                </div>
+            ) : (
+                <p>Enter a room to chat with other users, and do not let users know your chat room key.</p>
+            )}
+        </div>
     );
 };
 
