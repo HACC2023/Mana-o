611db87f5ad46e5e60e785c034dee38dc9f42560
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-11-11 21:38:16 -1000

    update for chat with friends

--- src/components/Chatting.js
+++ src/components/Chatting.js
@@ -5,22 +5,10 @@ import './Chatting.css';
 import AuthService from '../services/auth.service';
 import {
     MDBCol,
-    MDBContainer,
     MDBRow,
-    MDBCard,
-    MDBCardText,
-    MDBCardBody,
-    MDBCardImage,
-    MDBBtn,
     MDBBreadcrumb,
     MDBBreadcrumbItem,
-    MDBProgress,
-    MDBProgressBar,
-    MDBIcon,
-    MDBListGroup,
-    MDBListGroupItem
 } from 'mdb-react-ui-kit';
-
 
 const ENDPOINT = 'http://localhost:8080';
 
@@ -35,17 +23,25 @@ const Chatting = () => {
     const user = currentUser.email;
     const userName = `${currentUser.first_name} ${currentUser.last_name}`;
 
-    useEffect(() => {
-        const interval = setInterval(() => setValue(new Date()), 1000);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
+    const [friendEmail, setFriendEmail] = useState('');
+    const [chatTitle, setChatTitle] = useState('');
+    const [emailOrRoom, setEmailOrRoom] = useState('');
+    let updatedRoom="";
 
     const joinRoom = () => {
-        if (room !== "") {
-            socket.emit('join room', room);
+
+        if (room !== '' || friendEmail !== '') {
+            if (room === '') {
+                updatedRoom =
+                    friendEmail < user
+                        ? `${friendEmail}_${user}`
+                        : `${user}_${friendEmail}`;
+                setChatTitle(friendEmail); // Set chat title to friend's name
+            } else {
+                updatedRoom = room;
+            }
+            setEmailOrRoom(updatedRoom);
+            socket.emit('join room', updatedRoom);
             setShowChat(true);
         }
     };
@@ -72,11 +68,14 @@ const Chatting = () => {
     const handleSendMessage = async () => {
         if (socket && inputMessage.trim() !== '') {
             const messageData = {
-                room: room,
+                room: emailOrRoom,
                 author: user,
                 username: userName,
                 messages: inputMessage,
-                time: new Date(Date.now()).getHours() + ':' + String(new Date(Date.now()).getMinutes()).padStart(2, '0'),
+                time:
+                    new Date(Date.now()).getHours() +
+                    ':' +
+                    String(new Date(Date.now()).getMinutes()).padStart(2, '0'),
             };
 
             // Send the message to the server along with the room name
@@ -89,89 +88,95 @@ const Chatting = () => {
     };
 
     return (
-        <div className="chat-container">
-            {/* Friends List on the left side */}
-            <div className="friends-list">
-                <MDBRow>
-                    <MDBCol>
-                        <MDBBreadcrumb className="bg-light rounded-3 p-3 mb-4">
-                            <MDBBreadcrumbItem>
-                              <h1>Join a room</h1>
-                            </MDBBreadcrumbItem>
-                        </MDBBreadcrumb>
-                    </MDBCol>
-                </MDBRow>
-                <div className="input-container d-flex align-items-center"> {/* Added "d-flex" and "align-items-center" classes */}
-                    <input
-                        type="text"
-                        placeholder="Enter room name"
-                        value={room}
-                        onChange={(event) => {
-                            setRoom(event.target.value);
-                        }}
-                    />
-                    <button className="btn btn-primary join-button" onClick={joinRoom}>
-                        Join
-                    </button>
-                </div>
-                <hr  />
-                <MDBRow>
-                    <MDBCol>
-                        <MDBBreadcrumb className="bg-light rounded-3 p-3 mb-4">
-                            <MDBBreadcrumbItem>
-                                <h1>Chat with Friend</h1>
-                            </MDBBreadcrumbItem>
-                        </MDBBreadcrumb>
-                    </MDBCol>
-                </MDBRow>
-                <div className="input-container d-flex align-items-center"> {/* Added "d-flex" and "align-items-center" classes */}
-                    <input
-                        type="text"
-                        placeholder="Enter friend's Email"
-                        value={room}
-                        onChange={(event) => {
-                            setRoom(event.target.value);
-                        }}
-                    />
-                    <button className="btn btn-primary join-button" onClick={joinRoom}>
-                        Join
-                    </button>
-                </div>
-            </div>
+        <div>
+            <h1>Message</h1>
 
-            {/* Chat Messages */}
-            {showChat ? (
-                <div className="chat-messages">
-                    <h1>Room {room}</h1>
-                    <div className="message-container">
-                        {messageList.map((message) => (
-                            <div
-                                key={message.id}
-                                className={`message ${message.author === user ? 'own-message' : 'other-message'}`}
-                            >
-                                <div className="message-header">
-                                    <span className="username">{message.username}</span>
-                                    <span className="time">{message.time}</span>
-                                </div>
-                                <div className="message-content">{message.messages}</div>
-                            </div>
-                        ))}
-                    </div>
-
-                    {/* Message Input and Send Button */}
-                    <div className="message-input">
+            <div className="chat-container">
+                {/* Friends List on the left side */}
+                <div className="friends-list">
+                    <MDBRow>
+                        <MDBCol>
+                            <MDBBreadcrumb className="bg-light rounded-3 p-3 mb-4">
+                                <MDBBreadcrumbItem>
+                                    <h1>Join a room</h1>
+                                </MDBBreadcrumbItem>
+                            </MDBBreadcrumb>
+                        </MDBCol>
+                    </MDBRow>
+                    <div className="input-container d-flex align-items-center">
                         <input
                             type="text"
-                            placeholder="Type message here"
-                            value={inputMessage}
-                            onChange={(e) => setInputMessage(e.target.value)}
+                            placeholder="Enter room name"
+                            value={room}
+                            onChange={(event) => {
+                                setRoom(event.target.value);
+                            }}
                         />
-                        <button onClick={handleSendMessage}>Send</button>
+                        <button className="btn btn-primary join-button" onClick={joinRoom}>
+                            Join
+                        </button>
+                    </div>
+                    <hr />
+                    <MDBRow>
+                        <MDBCol>
+                            <MDBBreadcrumb className="bg-light rounded-3 p-3 mb-4">
+                                <MDBBreadcrumbItem>
+                                    <h1>Chat with Friend</h1>
+                                </MDBBreadcrumbItem>
+                            </MDBBreadcrumb>
+                        </MDBCol>
+                    </MDBRow>
+                    <div className="input-container d-flex align-items-center">
+                        <input
+                            type="text"
+                            placeholder="Enter friend's Email"
+                            value={friendEmail}
+                            onChange={(event) => {
+                                setFriendEmail(event.target.value);
+                            }}
+                        />
+                        <button className="btn btn-primary join-button" onClick={joinRoom}>
+                            Chat
+                        </button>
                     </div>
                 </div>
-            ) : (
-                 <></>
-            )}
+
+                {/* Chat Messages */}
+                {showChat ? (
+                    <div className="chat-messages">
+                        {friendEmail!==""? ( <h1>{chatTitle}</h1>): (<h1>Room {room}</h1>)}
+                        <div className="message-container">
+                            {messageList.map((message) => (
+                                <div
+                                    key={message.id}
+                                    className={`message ${
+                                        message.author === user ? 'own-message' : 'other-message'
+                                    }`}
+                                >
+                                    <div className="message-header">
+                                        <span className="username">{message.username}</span>
+                                        <span className="time">{message.time}</span>
+                                    </div>
+                                    <div className="message-content">{message.messages}</div>
+                                </div>
+                            ))}
+                        </div>
+
+                        {/* Message Input and Send Button */}
+                        <div className="message-input">
+                            <input
+                                type="text"
+                                placeholder="Type message here"
+                                value={inputMessage}
+                                onChange={(e) => setInputMessage(e.target.value)}
+                            />
+                            <button onClick={handleSendMessage}>Send</button>
+                        </div>
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </div>
         </div>
     );
 };
